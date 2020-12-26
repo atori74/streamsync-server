4b346d73225b0d0216060afd872f20bb0839f2e6
Atori <x90.empty@outlook.com>
2020-12-26 23:35:21 +0900

    comment: how to use frame

--- frame.go
+++ frame.go
@@ -7,8 +7,15 @@ type Frame struct {
 }
 
 /*
+### Receive
 再生地点
 {"from": "host", "type": "playbackPosition", "data": {"position": 11123, "currentTime": "2020-12-25 12:00:00"}}
+同期ストップ
+{"from": "client", "type": "command", "data": {"command": "syncStop"}}
+同期スタート
+{"from": "client", "type": "command", "data": {"command": "syncStart"}}
+
+### Send
 ルームを立てた際のルームIDの通知
 {"from": "server", "type": "roomInfo", "data": {"roomID": "fgk3f79bgg"}}
 クライアントの参加通知
